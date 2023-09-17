839d06f279463e37f290ad66e13c1032fd33ff01
Shiva <tdp511a@gmail.com>
2023-09-17 14:17:04 +0200

    commit the initial code

---
+++ machine_tracking/src/main/java/com/johndeere/machine_tracking/MachineTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.johndeere.machine_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineTrackingApplication.class, args);
+	}
+
+}
---
+++ machine_tracking/src/test/java/com/johndeere/machine_tracking/MachineTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johndeere.machine_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
